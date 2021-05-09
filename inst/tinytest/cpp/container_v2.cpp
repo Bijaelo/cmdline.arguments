2b51c24f939a2dcf3b1833d3674f5cce49d4aae2
Bijaelo <oliver.p.madsen@gmail.com>
2021-05-09 16:33:56 +0200

    finishing up v2 of cotainer

--- inst/tinytest/cpp/container_v2.cpp
+++ inst/tinytest/cpp/container_v2.cpp
@@ -1,4 +1,7 @@
-
+/*
+ * Test for rebuilding argument container using string_view containers.
+ * Currently V1 did not succesfully
+ */
 
 // [[Rcpp::plugins("cpp2a")]]
 
@@ -6,16 +9,130 @@
 
 #include <Rcpp.h>
 #include <unordered_set>
+#include <unordered_map>
 #include <string>
 #include <string_view>
 #include <list>
 #include <cmd_args/parser/argument/argument.h>
+#include <cmd_args/parser/argument/locator.h>
+#include <cmd_args/utils/options.h>
 
+using namespace cmd_args::utils;
 using namespace cmd_args::parser::argument;
 using namespace std;
-struct container{
-  list<cmd_argument> args;
+struct argument_container{
+private:
   unordered_set<string_view> flags;
+  unordered_multimap<string_view, cmd_argument&> mapper;
+  list<cmd_argument> args;
+  const bool allow_unknown_flags{false}, allow_duplicate_flags{true};
 
 
+  cmd_args::parser::argument_locator locator;
+  // Flag for handling if the same flag is provided muliple time?
+  multi_flag_handling option{"Invalid option provided to parser for handling multiple flags"};
+
+public:
+  argument_container() {};
+  argument_container(bool allow_unknown_flags, bool allow_duplicate_flags)
+      : allow_unknown_flags(allow_unknown_flags),
+        allow_duplicate_flags(allow_duplicate_flags){};
+
+
+  template<typename T, typename O, typename F>
+  inline void add_argument(const T& name,
+                           const F& flags, // Note flags is here, so we need  to go this -> flag for all assignments
+                           const T& meta,
+                           const T& action,
+                           const O& option,
+                           const T& choices){
+    /* Add argument takes the same parameters as the initializer of cmd_args::parser::argument::cmd_argument
+     * It creates a new argument,
+     * adds it to the back of the private member args,
+     * goes through the flags contained in the created argument checking for duplicates (throwig an error if one is foud and allow_duplicate_flags is false)
+     * and adds each flag to the private member set "flags" if no duplicate flags are found.
+     * after that is done, a mapper from the argument to the result slot is placed in mapper.
+     */
+    args.emplace_back(name, flags, meta, action, option, choices);
+    auto& a = args.back();
+    auto& f = a.flags();
+    auto e = f.end();
+    if(!allow_duplicate_flags){
+      for(auto b = f.begin(); b != e; b++){
+        if((this -> flags).find(*b) == (this -> flags).end()){
+          (this -> flags).emplace(*b);
+        }else{
+          for(auto b1 = f.begin(); b1 != b; b1++){
+            (this -> flags).erase(*b1);
+          }
+          args.pop_back();
+          Rcpp::stop("Duplicate flag");
+        }
+      }
+    }else{
+      for(auto b = f.begin(); b != e; b++)
+        (this -> flags).emplace(*b);
+    }
+    mapper.insert({a.name(), a});
+  }
+  const unordered_set<string_view>& getflags() const {
+    return flags;
+  }
+  const list<cmd_argument>& getargs() const {
+    return args;
+  }
+  const unordered_multimap<string_view, cmd_argument&> getmapping() const {
+    return mapper;
+  }
 };
+
+
+// [[Rcpp::export]]
+XPtr<argument_container> test_container_default_initializer(){
+  return XPtr<argument_container>(new argument_container());
+}
+
+
+// [[Rcpp::export]]
+XPtr<argument_container> test_container_initializer(bool allow_unknown_flags, bool allow_duplicate_flags){
+  return XPtr<argument_container>(new argument_container(allow_unknown_flags, allow_duplicate_flags));
+}
+
+
+// [[Rcpp::export]]
+void test_add_argument(XPtr<argument_container> Xptr,
+                       const SEXP& name,
+                       const SEXP& flags,
+                       const SEXP& meta,
+                       const SEXP& action,
+                       const SEXP& option,
+                       const SEXP& choices){
+  Xptr -> add_argument(name,
+                       flags,
+                       meta,
+                       action,
+                       option,
+                       choices);
+}
+
+// [[Rcpp::export]]
+list<string> test_getflags(XPtr<argument_container> Xptr){
+  list<string> out;
+  auto flags = Xptr -> getflags();
+  auto e = flags.end();
+  for(auto b = flags.begin(); b != e; b++)
+    out.push_back(string(*b));
+  return out;
+}
+
+// [[Rcpp::export]]
+size_t test_nargs(XPtr<argument_container> Xptr){
+  return (Xptr -> getargs()).size();
+}
+
+// [[Rcpp::export]]
+size_t test_nmappings(XPtr<argument_container> Xptr, string name){
+  auto mapp = Xptr -> getmapping();
+  return mapp.count(string_view(name));
+}
+
--- inst/tinytest/test_container.R
+++ inst/tinytest/test_container.R
@@ -1,16 +1,18 @@
-if(is.null(Sys.getenv('CMD_ARG_ALL_TEST')))
+ if(is.null(Sys.getenv('CMD_ARG_ALL_TEST')))
   exit_file("Set CMD_ARG_ALL_TEST environment variable to run these tests in this file.")
 
 # Tinytest for <cmd_args/parser/argument/container.h>
 
-Rcpp::sourceCpp("cpp/container.cpp")
+
+Rcpp::sourceCpp("cpp/container_v2.cpp")
+
 
 ptr1 <- test_container_initializer(FALSE, TRUE)
 
 # 2 and 3 should be identical.
 ptr2 <- test_container_initializer(FALSE, FALSE)
 
-ptr3 <- test_container_default_initializer()
+
 
 arg1 <- list(name = 'test',
              flags = c('-t', '--test'),
@@ -24,7 +26,7 @@ arg2 <- list(name = "foo",
              action = 'store_true',
              option = 'v',
              choices = NULL)
-
+library(tinytest)
 expect_silent(test_add_argument(ptr1,
                                 arg1[["name"]],
                                 arg1[["flags"]],
@@ -32,14 +34,34 @@ expect_silent(test_add_argument(ptr1,
                                 arg1[["action"]],
                                 arg1[["option"]],
                                 arg1[["choices"]]))
+expect_silent(test_add_argument(ptr1,
+                                arg2[["name"]],
+                                arg2[["flags"]],
+                                arg2[["meta"]],
+                                arg2[["action"]],
+                                arg2[["option"]],
+                                arg2[["choices"]]))
 
+test_getflags(ptr1)
+test_nargs(ptr1)
+test_nmappings(ptr1, 'test')
 expect_silent(test_add_argument(ptr2,
-                  arg1[["name"]],
-                  arg1[["flags"]],
-                  arg1[["meta"]],
-                  arg1[["action"]],
-                  arg1[["option"]],
-                  arg1[["choices"]]))
+                                arg1[["name"]],
+                                arg1[["flags"]],
+                                arg1[["meta"]],
+                                arg1[["action"]],
+                                arg1[["option"]],
+                                arg1[["choices"]]))
+expect_silent(test_add_argument(ptr2,
+                                arg2[["name"]],
+                                arg2[["flags"]],
+                                arg2[["meta"]],
+                                arg2[["action"]],
+                                arg2[["option"]],
+                                arg2[["choices"]]))
+
+test_getflags(ptr2)
+test_nargs(ptr2)
 
 expect_silent(test_add_argument(ptr3,
                   arg1[["name"]],
